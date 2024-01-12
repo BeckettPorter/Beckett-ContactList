6c8f3788ea855e8822ce7528377fbf44085f90fb
BeckettPorter <porterbeckett@gmail.com>
2024-01-12 09:34:16 -0800

    Test Commit

---
+++ src/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+
+    public Person(String firstName, String lastName, String phoneNumber)
+
+}
